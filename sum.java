5b556ff67b14fdfa8fb197f89d12959e8110b2e4
lokesh rajendra khadse <lokeshkhadse19@gmail.com>
2023-05-10 22:32:00 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class sum{
+    public static void main(String[] args){
+        int a=5;
+        int b=4;
+         
+        int c=a+b;
+        System.out.println(c);
+    
+    }
+}
