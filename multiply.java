f711920ea86e90ab7cee7bfbfdb2a4fe816846e9
lokesh rajendra khadse <lokeshkhadse19@gmail.com>
2023-05-11 00:49:11 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class multiply {
+    public static void main(String[] args){
+    int a=2;
+    int b=3;
+    int c=a*b;
+    System.out.println("sum is " +c);
+    }
+    
+}
